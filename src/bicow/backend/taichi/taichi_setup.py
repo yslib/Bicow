60fd53c27712c40a32b3e4900f586b7e48e449a1
Shuoliu Yang <visysl@outlook.com>
2021-08-16 14:38:45 +0800

    update taichi setup to 0.7.29

--- src/bicow/backend/taichi/taichi_setup.py
+++ src/bicow/backend/taichi/taichi_setup.py
@@ -1,7 +1,7 @@
 import taichi as ti
 
 def taichi_init():
-    ti.init(arch=ti.cpu, device_memory_fraction=0.5)
+    ti.init(arch=ti.gpu,packed=True,device_memory_fraction=0.5) 
 
 def taichi_shutdown():
     pass
